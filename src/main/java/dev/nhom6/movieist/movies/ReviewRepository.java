cbf345f68776441add6858702c9a44aae556f863
Quocc1 <75513474+Quocc1@users.noreply.github.com>
2023-12-04 13:08:06 +0700

    first commit

---
+++ src/main/java/dev/nhom6/movieist/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.nhom6.movieist.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
